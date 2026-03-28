27f0dddfbd4b3e1586473b0f66d44c72ac31fdbb
Yusuf Bahtiyar <yusuf.kotacom@gmail.com>
2026-03-28 23:42:48 +0700

    feat(web): add dedicated new page content view without video gen form

---
+++ project/aitoearn-web/src/app/[lng]/new-page-content/page.tsx
@@ -0,0 +1,37 @@
+/**
+ * New Page Content - dedicated content management page without video generation form
+ */
+
+import dynamic from 'next/dynamic'
+import { useTranslation } from '@/app/i18n'
+import { fallbackLng, languages } from '@/lib/i18n/languageConfig'
+import { getMetadata } from '@/utils/general'
+
+interface PageParams {
+  params: Promise<{ lng: string }>
+}
+
+export async function generateMetadata({ params }: PageParams) {
+  let { lng } = await params
+  if (!languages.includes(lng))
+    lng = fallbackLng
+  const { t } = await useTranslation(lng, 'common')
+
+  return getMetadata(
+    {
+      title: `New Page Content - ${t('header.draftBox')}`,
+      description: t('header.draftBoxSeoDescription'),
+      keywords: t('header.draftBoxSeoKeywords'),
+    },
+    lng,
+    '/new-page-content',
+  )
+}
+
+const DraftBoxCore = dynamic(() => import('../draft-box/DraftBoxCore'), {
+  ssr: false,
+})
+
+export default function NewPageContentPage() {
+  return <DraftBoxCore showGenerateBar={false} />
+}
